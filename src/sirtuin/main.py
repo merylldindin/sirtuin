f964234f469afd8e027a545360aabefeec150cbc
Coricos <meryll_dindin@berkeley.edu>
2022-11-06 18:55:49 -0800

    feat: prepare cli skeleton

--- src/sirtuin/main.py
+++ src/sirtuin/main.py
@@ -5,13 +5,52 @@ from sirtuin.controllers.http_headers import print_content_security_policy
 cli = typer.Typer()
 
 
+# ? HTTP Headers
+
+
 @cli.command()
-def generate_content_security_policy(
+def generate_csp(
     filepath: str = typer.Argument(default="sirtuin.toml"),
 ) -> None:
     print_content_security_policy(filepath)
 
 
+# ? Elastic Beanstalk
+
+
 @cli.command()
-def test(filepath: str = typer.Argument(default="sirtuin.toml")) -> None:
-    print_content_security_policy(filepath)
+def create_beanstalk(
+    filepath: str = typer.Argument(default="sirtuin.toml"),
+) -> None:
+    pass
+
+
+@cli.command()
+def upgrade_beanstalk(
+    filepath: str = typer.Argument(default="sirtuin.toml"),
+) -> None:
+    pass
+
+
+@cli.command()
+def deploy_beanstalk(
+    filepath: str = typer.Argument(default="sirtuin.toml"),
+) -> None:
+    pass
+
+
+@cli.command()
+def terminate_beanstalk(
+    filepath: str = typer.Argument(default="sirtuin.toml"),
+) -> None:
+    pass
+
+
+# ? Cloudfront
+
+
+@cli.command()
+def deploy_cloudfront(
+    filepath: str = typer.Argument(default="sirtuin.toml"),
+) -> None:
+    pass
