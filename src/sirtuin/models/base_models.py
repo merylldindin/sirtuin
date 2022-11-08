9318a0fade247bdb7e118a112749641bd465ffb0
Coricos <meryll_dindin@berkeley.edu>
2022-11-08 15:15:18 -0800

    build: enable mypy support

--- src/sirtuin/models/base_models.py
+++ src/sirtuin/models/base_models.py
@@ -19,7 +19,7 @@ def _sanitize_value(
 
 
 class SanitizedBaseModel(BaseModel):
-    def dict(self, *, use_sanitization: bool = False, **kwargs) -> dict[str, Any]:  # type: ignore
+    def dict(self, *, use_sanitization: bool = False, **kwargs) -> dict[str, Any]:  # type: ignore # noqa: E501
         base_object = super().dict(**kwargs)
 
         return (
