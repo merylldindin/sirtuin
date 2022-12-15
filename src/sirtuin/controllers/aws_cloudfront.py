703274e1b851e70104b9cf6fc244e2beaa39bfe2
Coricos <meryll_dindin@berkeley.edu>
2022-12-14 20:49:45 -0800

    fix: account for subprocess split and string management

--- src/sirtuin/controllers/aws_cloudfront.py
+++ src/sirtuin/controllers/aws_cloudfront.py
@@ -27,7 +27,7 @@ def _copy_application_bundle_to_bucket(config: CloudfrontSirtuinConfig) -> str:
         f"aws "
         f"s3 cp {config.application.bundle} "
         f"s3://{config.bucket.name} "
-        f"--recursive --content-type 'text/html' --exclude '*.*' "
+        f"--recursive --content-type text/html --exclude *.* "
         f"--region {config.bucket.region.value} "
         + (f"--profile {config.profile}" if config.profile is not None else "")
     )
@@ -43,7 +43,7 @@ def invalidate_cloudfront_distribution(
         f"aws "
         f"cloudfront create-invalidation "
         f"--distribution-id {config.cloudfront.distribution} "
-        f"--paths '/*' "
+        f"--paths /* "
         f"--region {config.bucket.region.value} "
         + (f"--profile {config.profile}" if config.profile is not None else "")
     )
