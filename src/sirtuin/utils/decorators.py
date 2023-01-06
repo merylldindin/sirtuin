fe4d0c629d00bc9332cd189386ef86b252bc7378
merylldindin <merylldin@gmail.com>
2023-01-06 16:21:27 +0100

    feat: handle usecase of env variable with space

--- src/sirtuin/utils/decorators.py
+++ src/sirtuin/utils/decorators.py
@@ -19,6 +19,10 @@ def if_exists(function: Callable[[Path], T]) -> Callable[[Path], T]:
     return wrapper
 
 
+def _format_command(command: str) -> list[str]:
+    return [item.replace("\\", " ") for item in command.split(" ")]
+
+
 def run_command(
     description: str,
 ) -> Callable[..., Callable[[T], str | CompletedProcess[Any]]]:
@@ -30,7 +34,7 @@ def run_command(
                 return function(*args, **kwargs)
 
             if kwargs["verbose"]:
-                return subprocess.run(function(*args, **kwargs).split(" "))
+                return subprocess.run(_format_command(function(*args, **kwargs)))
 
             with Progress(
                 SpinnerColumn(),
@@ -40,7 +44,7 @@ def run_command(
                 spinner.add_task(description=description, total=None)
 
                 return subprocess.run(
-                    function(*args, **kwargs).split(" "),
+                    _format_command(function(*args, **kwargs)),
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.STDOUT,
                 )
