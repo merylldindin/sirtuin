4540e326edee01e75cb344f29899e2b1ae74d26c
merylldindin <merylldin@gmail.com>
2023-01-19 18:26:10 +0100

    fix: make load balancer creation command

--- src/sirtuin/controllers/aws_compute.py
+++ src/sirtuin/controllers/aws_compute.py
@@ -1,7 +1,9 @@
 from sirtuin.models.aws_compute import LoadBalancerType
 from sirtuin.models.aws_regions import AwsRegion
+from sirtuin.utils.decorators import run_command
 
 
+@run_command(description="Create Elastic Load Balancer")
 def create_load_balancer_from_prompt(
     name: str, type: str, subnets: str, region: str, profile: str, verbose: bool = False
 ) -> str:
